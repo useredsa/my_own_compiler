7c9b6f74afb6be6cdfa6b4dcb08f103ca23cdabe
Rvb0rob0t <ruboroba@gmail.com>
2020-05-12 15:02:56 +0200

    Syntactic logger added, bison parser improved and better output in case of error

--- src/log.cpp
+++ src/log.cpp
@@ -6,9 +6,10 @@ namespace compiler {
 // It would be wise to move the reference of the log to a late initialization.
 // Maybe, a program class could be created that managed initialization instead
 // of using global variables.
-BasicLogger internal_log(std::cerr, "Internal Error: ");
-BasicLogger semantic_log(std::cerr, "Semantic Error: ");
 BasicLogger lexical_log(std::cerr, "Lexical Error: ");
+BasicLogger syntactic_log(std::cerr, "Syntax Error: ");
+BasicLogger semantic_log(std::cerr, "Semantic Error: ");
 BasicLogger warning_log(std::cerr, "Warning: ");
+BasicLogger internal_log(std::cerr, "Internal Error: ");
 
 } // namespace compiler
